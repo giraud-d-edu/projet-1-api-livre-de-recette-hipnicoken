029212ced953a1f395459a829ff070f5d899ce57
azznek <kenzadahhmani@gmail.com>
2025-03-06 18:17:33 +0100

    enfin

--- src/controllers/ingredient.controller.ts
+++ src/controllers/ingredient.controller.ts
@@ -14,7 +14,8 @@ export const IngredientController = {
   },
 
   async getById(ctx: Context) {
-    const id = ctx.request.url.searchParams.get("id");
+    const id = ctx.params.id; // ✅ Récupérer l'ID depuis l'URL dynamique "/ingredients/:id"
+    
     if (!id) {
         ctx.response.status = 400;
         ctx.response.body = { error: "ID requis" };
